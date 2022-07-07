e41f31d813f58ce724dd1720d1f8407d91bae561
Victor Jansen <victorfjansen@gmail.com>
2022-07-06 22:38:10 -0300

    added the recipe component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StructuralModule } from './structural/structural.module';
+import { RecipeComponent } from './components/recipe/recipe.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, RecipeComponent],
   imports: [BrowserModule, AppRoutingModule, StructuralModule],
   providers: [],
   bootstrap: [AppComponent],
