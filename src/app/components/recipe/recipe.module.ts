019486a783f0b1b72ca446c131d93ad340c02af9
Victor Jansen <victorfjansen@gmail.com>
2022-07-06 22:48:42 -0300

    created recipelist component

--- src/app/components/recipe/recipe.module.ts
+++ src/app/components/recipe/recipe.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { RecipeComponent } from './recipe.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
 @NgModule({
-  declarations: [RecipeComponent],
+  declarations: [RecipeComponent, RecipeListComponent],
   exports: [RecipeComponent],
   imports: [CommonModule],
 })
