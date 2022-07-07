c2ca34fd804add750d450e57688bce8fdd614d65
Victor Jansen <victorfjansen@gmail.com>
2022-07-06 22:57:42 -0300

    structuring shopping module

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,9 +2,16 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { RecipeModule } from './recipe/recipe.module';
+import { ShoppingComponent } from './shopping/shopping.component';
+import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping/shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ShoppingComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
+  ],
   imports: [CommonModule, RecipeModule],
 })
 export class ComponentsModule {}
