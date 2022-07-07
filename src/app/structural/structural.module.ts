998d0fda183aa09f922ba59314b444579ad26937
Victor Jansen <victorfjansen@gmail.com>
2022-07-06 22:16:38 -0300

    organizing the modules and adding the header component in app-module

---
+++ src/app/structural/structural.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { HeaderModule } from './header/header.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, HeaderModule],
+})
+export class StructuralModule {}
